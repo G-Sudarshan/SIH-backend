86dcda2a0e5683881ba1a7239be97510e5d50291
harshal239 <harshalwalunj49@gmail.com>
2022-08-15 22:56:00 +0530

    Added CSV Schema and Controller

--- src/controllers/CSVController.js
+++ src/controllers/CSVController.js
@@ -1,10 +1,34 @@
 const csvToJson = require("csvtojson");
+const CSV = require("../models/CSV");
 
 const AddUser = async (req, res) => {
   try {
-    const jsonArray = await csvToJson().fromFile("sample.csv");
-    console.log(jsonArray);
-    res.json({ message: "Hello" });
+    const jsonArray = await csvToJson().fromFile(
+      `${__dirname}/./placement_data.csv`
+    );
+
+    for (let index = 0; index < jsonArray.length; index++) {
+      if (jsonArray[index].minority === "Yes") {
+        jsonArray[index].minority = true;
+      } else {
+        jsonArray[index].minority = false;
+      }
+      jsonArray[index].ctcOffered = parseFloat(jsonArray[index].ctcOffered);
+    }
+    const records = new CSV({
+      collegeId: "62fa2aece66cf0d26ca4bfda",
+      placementData: jsonArray,
+      year: 2022,
+    });
+    await records.save();
+
+    res.status(200).json({
+      status: 200,
+      message: "Placement data inserted Successfully !",
+      body: {
+        records,
+      },
+    });
   } catch (error) {
     console.log({ Error: error });
   }
--- src/models/CSV.js
+++ src/models/CSV.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
 
-const CSVSchema = new mongoose.Schema({
+const CSVRow = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
-  rollNum: {
+  rollNumber: {
+    type: String,
+    required: true,
+  },
+  mobileNumber: {
+    type: String,
+    required: true,
+  },
+  emailID: {
     type: String,
     required: true,
   },
@@ -19,15 +27,34 @@ const CSVSchema = new mongoose.Schema({
   },
   companyType: {
     type: String,
-    enum: ["Startup", "Fintech", "Product", "Consultant"],
+    enum: ["Startup", "Fintech", "Product", "Consultant", "Other"],
     required: true,
   },
-  ctcOfferred: {
+  ctcOffered: {
     type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["Male", "Female", "Other"],
+    required: true,
+  },
+  minority: {
+    type: Boolean,
     required: true,
   },
   program: {
     type: String,
+    enum: [
+      "Engineering and Technology",
+      "Applied arts and crafts",
+      "Architecture",
+      "Town planning",
+      "Hotel management and catering",
+      "Management",
+      "MCA",
+      "Pharmacy",
+    ],
     required: true,
   },
   branch: {
@@ -36,11 +63,24 @@ const CSVSchema = new mongoose.Schema({
   },
   status: {
     type: String,
-    enum: ["Employed", "Unemployed", "Higher Studies"],
+    enum: ["Employed", "Unemployed", "Higher studies"],
     required: true,
   },
   category: {
     type: String,
+    enum: ["Open", "OBC", "SC", "ST", "Other"],
+    required: true,
+  },
+});
+
+const CSVSchema = new mongoose.Schema({
+  collegeId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "College",
+  },
+  placementData: [CSVRow],
+  year: {
+    type: Number,
     required: true,
   },
 });
