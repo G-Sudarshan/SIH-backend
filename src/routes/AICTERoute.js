7c3a09e21a97ec57e46cebe13d5fb01a28c5d76e
G Sudarshan's macbook air <11sudarshang@gmail.com>
2022-08-15 23:18:56 +0530

    getReasonsFreaquencyMap API endpoint for loading graph implemented

--- src/routes/AICTERoute.js
+++ src/routes/AICTERoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { GetReasons } = require("../controllers/AICTEController")
+const { GetReasons, GetReasonsFreaquencyMap } = require("../controllers/AICTEController")
 
 const AICTERouter = express.Router();
 
 AICTERouter.get("/getReasons", GetReasons);
+AICTERouter.get("/getReasonsFreaquencyMap",GetReasonsFreaquencyMap);
 
 module.exports = AICTERouter;
