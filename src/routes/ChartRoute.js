41e4214ae198cfa44bae90e84263f8bbf12b58de
harshal239 <harshalwalunj49@gmail.com>
2022-08-21 12:54:24 +0530

    Added yearWisePlacement graph

--- src/routes/ChartRoute.js
+++ src/routes/ChartRoute.js
@@ -5,11 +5,14 @@ const {
   programWisePlacement,
   stateWisePlacement,
   institutionTypeWisePlacement,
-
+  categoryWiseEnrollment,
+  yearWisePlacement,
 } = require("../controllers/ChartController");
 
 ChartRouter.get("/placedUnplacedGraph/:id", PlacedUnplacedGraph);
 ChartRouter.get("/programWisePlacement", programWisePlacement);
 ChartRouter.get("/stateWisePlacement", stateWisePlacement);
 ChartRouter.get("/institutionTypeWisePlacement", institutionTypeWisePlacement);
+ChartRouter.get("/categoryWiseEnrollment", categoryWiseEnrollment);
+ChartRouter.get("/yearWisePlacement", yearWisePlacement);
 module.exports = ChartRouter;
